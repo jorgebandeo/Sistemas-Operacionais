4380339ce36e0bfceb1c7b2a1d017103b6a14b19
Jorge Bandeo <jorgebandeo+Github@hotmail.com>
2023-05-16 02:47:42 -0300

    concerto da logica do rr_p

--- CodeBlox/schedule_rr_p.c
+++ CodeBlox/schedule_rr_p.c
@@ -16,7 +16,6 @@ void add_rp(char *name, int priority, int burst) {
     insert_task(&TASK_LIST_p, new_task);
 }
 
-// Troca a posição de duas tarefas na lista
 void swap(struct node *a, struct node *b) {
     struct Task *temp = a->task;
     a->task = b->task;
@@ -46,23 +45,53 @@ void sort_by_priority(struct node **head) {
         lptr = ptr1;
     } while (swapped);
 }
-
 // Executa o escalonamento Round-Robin com prioridade
 void schedule_rr_p() {
-    struct node *temp;
-    while (TASK_LIST_p != NULL) {
-        temp = TASK_LIST_p;
-        while (temp != NULL) {
-            sort_by_priority(&temp);
-            if (temp->task->burst > QUANTUM) {
-                run(temp->task, QUANTUM);
-                temp->task->burst -= QUANTUM;
-            } else {
-                run(temp->task, temp->task->burst);
-                delete_task(&TASK_LIST_p, temp->task);
-            }
-            temp = temp->next;
+    sort_by_priority(&TASK_LIST_p);///organiza o vetor de tesk do maior para o menor
+    while(TASK_LIST_p != NULL){
+
+        struct node *prioritarios = NULL;
+        Task *tranferen = TASK_LIST_p->task;
+        delete_task(&TASK_LIST_p,TASK_LIST_p->task );///remove o ultimo da lista
+        insert_task(&prioritarios,tranferen);///incluir o ultimo que é o de maior prioridade
+
+        while(prioritarios->task->priority == TASK_LIST_p->task->priority){///Ve se o novo proximo tem a mesma prioridade
+            Task *tranfer = TASK_LIST_p->task;
+            delete_task(&TASK_LIST_p,TASK_LIST_p->task );///remove o ultimo da lista
+            insert_task(&prioritarios,tranfer );///incluir o ultimo que é o de maior prioridade
         }
+
+        while(prioritarios!=NULL){/// comesa o RR
+            struct node *inicio = prioritarios;//pega o final da fila
+
+            while(inicio->next !=NULL){//movimenta ate o inicio
+                inicio = inicio->next;
+            }
+
+            int slince_cont = 0;
+            Task *tranfer = inicio->task;//pega a primaira tes da fila
+
+            ///pega o menor slince possivel
+            if (tranfer->burst > QUANTUM){
+                slince_cont = QUANTUM;
+            }else{
+                slince_cont = tranfer->burst;
+            }
+
+            run(tranfer, slince_cont);///execunta tarefa
+            tranfer->burst -= QUANTUM;
+
+            if (tranfer->burst <= 0){///se meu tempo foi atingido deleta
+                delete_task(&prioritarios,tranfer);
+            }else{///se não retona para o fim da fila
+                delete_task(&prioritarios,tranfer);
+                insert_task(&prioritarios,tranfer);
+
+            }
+
+        }
+
     }
+
 }
 
